c78676c17e6b90cc315152e0f7f60573597020cc
Awwab <mawwabkhank2006@gmail.com>
2024-02-07 11:38:41 +0500

    improved code a little bit

--- main.py
+++ main.py
@@ -11,28 +11,27 @@ params = {
     'lng': LNG,
     'formatted': 0
 }
-response = requests.get('https://api.sunrise-sunset.org/json', params=params)
-response.raise_for_status()
 
-sunrise = int(response.json()['results']['sunrise'].split('T')[-1].split(':')[0])
-sunset = int(response.json()['results']['sunset'].split('T')[-1].split(':')[0])
+def is_close():
+    response = requests.get('http://api.open-notify.org/iss-now.json')
+    response.raise_for_status()
 
-response = requests.get('http://api.open-notify.org/iss-now.json')
-response.raise_for_status()
-
-longitude = float(response.json()['iss_position']['longitude'])
-latitude = float(response.json()['iss_position']['latitude'])
-
-def is_close(iss_lat, iss_lng):
-    if abs(abs(LAT) - abs(iss_lat)) <=5 and abs(abs(LNG) - abs(iss_lng)):
+    longitude = float(response.json()['iss_position']['longitude'])
+    latitude = float(response.json()['iss_position']['latitude'])
+    if abs(abs(LAT) - abs(latitude)) <=5 and abs(abs(LNG) - abs(longitude)):
         return True
     return False
 
 def is_dark():
-    hour = dt.datetime.now().hour()
-    return (24 > hour >= 20 ) and (0 <= hour <= 5)
+    response = requests.get('https://api.sunrise-sunset.org/json', params=params)
+    response.raise_for_status()
 
-if is_close(latitude, longitude) and is_dark():
+    sunrise = int(response.json()['results']['sunrise'].split('T')[-1].split(':')[0])
+    sunset = int(response.json()['results']['sunset'].split('T')[-1].split(':')[0])
+    hour = dt.datetime.now().hour()
+    return (hour >= sunset) and (hour <= sunrise)
+
+if is_close() and is_dark():
     with smtplib.SMTP('smtp.gmail.com') as connection:
         connection.starttls()
         connection.login(user=MY_EMAIL, password=MY_PASSWORD)
